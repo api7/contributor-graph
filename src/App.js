ded33d73779111d79fcc86baeccaf07de8f1a7b7
litesun <7sunmiao@gmail.com>
2021-03-09 18:20:36 +0800

    feat: add contributor page

--- src/App.js
+++ src/App.js
@@ -1,56 +1,311 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+// import { useEffect, useState } from 'react';
+import { cloneDeep } from "lodash";
+import { FormControl, InputGroup, ListGroup, Button } from "react-bootstrap";
+import ReactECharts from "echarts-for-react";
+// import { CopyToClipboard } from "react-copy-to-clipboard";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import './App.css';
+const getMonths = (month = 12) => {
+  const d = new Date();
+  const result = [];
+  for (let i = 0; i < month; i++) {
+      d.setMonth(d.getMonth() - 1);
+      const m = d.getMonth() + 1;
+      const month = m < 10 ? `0${m}` : m;
+      result.push(`${d.getFullYear()}-${month}`);
+  }
+  return result.sort();
+};
+
+const DEFAULT_OPTIONS = {
+  title: {
+      text: "Contributor Growth Curve",
+  },
+  tooltip: {
+      trigger: "axis",
+  },
+  legend: {
+      data: [],
+  },
+  toolbox: {
+      feature: {
+          saveAsImage: {},
+      },
+  },
+  grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true,
+  },
+  xAxis: [
+      {
+          type: "category",
+          boundaryGap: false,
+          data: [],
+      },
+  ],
+  yAxis: [
+      {
+          type: "value",
+      },
+  ],
+  series: [],
+};
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
+  // const { host } = '';
+  const [loading, setLoading] = React.useState(false);
+  const [dataSource, setDataSource] = React.useState(
+      {}
+  );
+  const [activeDate, setActiveDate] = React.useState("1year");
+  const [xAxis, setXAxis] = React.useState([]);
+  const [legendData, setLegendData] = React.useState([]);
+  const [option, setOption] = React.useState(DEFAULT_OPTIONS);
+
+  // const router = useRouter();
+  const updateSeries = (xAxis) => {
+      const newClonedOption = cloneDeep(option);
+      const tmpLegendData = [];
+      Object.keys(dataSource || {}).map(key => {
+          const repo = key;
+          const filteredData = [];
+          const data = dataSource[key] || [];
+          if (!tmpLegendData.includes(key)) {
+              tmpLegendData.push(key);
+          }
+
+          xAxis.map(item => {
+              const index = data.findIndex(_item => {
+                  return (
+                      new Date(_item.date).getTime() >
+                      new Date(item).getTime()
+                  );
+              });
+              if (index !== -1 && data[index - 1]) {
+                  filteredData.push({
+                      date: item,
+                      contributorNum: data[index - 1].contributorNum,
+                  });
+              }
+          });
+
+          const findIndex = newClonedOption.series.findIndex(
+              (item) => item.name === repo
+          );
+          if (findIndex === -1) {
+              newClonedOption.series = [
+                  ...newClonedOption.series,
+                  {
+                      name: repo,
+                      type: "line",
+                      smooth: true,
+                      data: filteredData.map(item => item.contributorNum),
+                  },
+              ];
+          } else {
+              newClonedOption.series[findIndex] = {
+                  name: repo,
+                  type: "line",
+                  smooth: true,
+                  data: filteredData.map(item => item.contributorNum),
+              };
+          }
+      });
+      newClonedOption.xAxis[0] = {
+          type: "category",
+          boundaryGap: false,
+          data: xAxis,
+      };
+      newClonedOption.legend.data = tmpLegendData;
+      newClonedOption.legend.data = legendData;
+      setOption(newClonedOption);
+  };
+
+  const getData = (repo) => {
+      setLoading(true);
+      if (!legendData.includes(repo)) {
+          setLegendData(legendData.concat(repo));
+      }
+
+      fetch(`http://40.73.74.111:8080/contributors?repo=${repo}`)
+          .then(response => response.json())
+          .then(myJson => {
+              const { contributors = [] } = myJson;
+              const data = contributors.map((item) => ({
+                  contributorNum: item.idx,
+                  date: item.date
+              }));
+              // 计算当前月份的贡献者人数
+              setLoading(false);
+
+              const clonedDatasource = cloneDeep(dataSource);
+              if (!clonedDatasource[repo]) {
+                  setDataSource({ ...clonedDatasource, ...{ [repo]: data } });
+              }
+          });
+  };
+
+  React.useEffect(() => {
+      switch (activeDate) {
+          case "1month":
+              setXAxis(getMonths(1));
+              break;
+          case "3months":
+              setXAxis(getMonths(3));
+              break;
+          case "6months":
+              setXAxis(getMonths(6));
+              break;
+          case "1year":
+              setXAxis(getMonths(12));
+              break;
+          default:
+              break;
+      }
+  }, [activeDate]);
+
+  React.useEffect(() => {
+      updateSeries(xAxis);
+  }, [dataSource, xAxis]);
+
+  React.useEffect(() => {
+      // const { type, repo = "" } = router.query;
+      // const repoArr = repo.split(",");
+      // if (type === "contributor" && repo) {
+      //     repoArr.map(item => {
+      //         getData(item);
+      //     });
+      // }
   }, []);
+
+  const [selectedType, setSelectedType] = React.useState("#contributor");
+  const [repo, setRepo] = React.useState("");
+
   return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
+    <>
+    <div
+        className="content"
+        style={{
+            display: "flex",
+            width: "80%",
+            marginLeft: "10%",
+            flexDirection: "row",
+            minHeight: "700px",
+            justifyContent: "space-between",
+        }}
+    >
+        <div className="left" style={{ marginTop: "200px" }}>
+            <ListGroup defaultActiveKey={selectedType}>
+                <ListGroup.Item
+                    action
+                    href="#contributor"
+                    onClick={() => setSelectedType("#contributor")}
+                >
+                    Contributor Growth Curve
+                </ListGroup.Item>
+                <ListGroup.Item
+                    action
+                    href="#activity"
+                    onClick={() => setSelectedType("#activity")}
+                    disabled
+                >
+                    活跃度曲线
+                </ListGroup.Item>
+            </ListGroup>
+        </div>
+        <div
+            className="right"
+            style={{ width: "80%", marginTop: "2%" }}
         >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
+            <div
+                className="search-container"
+                style={{ display: "flex", justifyContent: "center" }}
+            >
+                <InputGroup>
+                    <FormControl
+                        placeholder="apache/apisix"
+                        aria-label="apache/apisix"
+                        aria-describedby="apache/apisix"
+                        value={repo}
+                        onChange={e => {
+                            setRepo(e.target.value);
+                        }}
+                    />
+                    <InputGroup.Append>
+                        <Button
+                            variant="primary"
+                            onClick={async () => {
+                                await getData(repo);
+                            }}
+                        >
+                            Add
+                        </Button>
+                        <>
+                            <Button
+                                variant="danger"
+                                onClick={() => {
+                                    setLegendData([""]);
+                                    setOption(DEFAULT_OPTIONS);
+                                }}
+                            >
+                                clear
+                            </Button>
+                            {/* <CopyToClipboard
+                                text={`${host}?type=contributor&repo=${legendData.join(',')}`} onCopy={(_, result) => { }}
+                            >
+                                <Button variant="success">share</Button>
+                            </CopyToClipboard> */}
+                        </>
+                    </InputGroup.Append>
+                </InputGroup>
+            </div>
+            <div id="chart" style={{ marginTop: "5%" }}>
+                <div style={{ marginBottom: "10px" }}>
+                    <Button
+                        variant="outline-primary" value="1month" active={activeDate === '1month'} onClick={(e) => {
+                            setActiveDate(e.currentTarget.value);
+                        }}
+                    >
+                        1 month
+                    </Button>{" "}
+                    <Button
+                        variant="outline-primary" value="3months" active={activeDate === '3months'} onClick={(e) => {
+                            setActiveDate(e.currentTarget.value);
+                        }}
+                    >
+                        3 months
+                    </Button>{" "}
+                    <Button
+                        variant="outline-primary" value="6months" active={activeDate === '6months'}
+                        onClick={(e) => {
+                            setActiveDate(e.currentTarget.value);
+                        }}
+                    >6 months
+                    </Button>
+                    {' '}
+                    <Button
+                        variant="outline-primary" value="1year" active={activeDate === '1year'} onClick={(e) => {
+                            setActiveDate(e.currentTarget.value);
+                        }}
+                    >
+                        1 year
+                    </Button>{" "}
+                </div>
+                <ReactECharts
+                    option={option}
+                    style={{ height: 700 }}
+                    showLoading={loading}
+                    notMerge
+                />
+            </div>
+        </div>
+    </div>
+</>
   );
 }
 
