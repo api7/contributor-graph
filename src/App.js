872ddb8c5270b2ee4f4b4fb35462926d388bee53
litesun <7sunmiao@gmail.com>
2021-03-09 19:08:32 +0800

    feat: update api addr

--- src/App.js
+++ src/App.js
@@ -132,7 +132,7 @@ function App() {
           setLegendData(legendData.concat(repo));
       }
 
-      fetch(`http://40.73.74.111:8080/contributors?repo=${repo}`)
+      fetch(`http://40.73.96.199:8080/contributors?repo=${repo}`)
           .then(response => response.json())
           .then(myJson => {
               const { contributors = [] } = myJson;
@@ -214,7 +214,7 @@ function App() {
                     onClick={() => setSelectedType("#activity")}
                     disabled
                 >
-                    活跃度曲线
+                    Activity Curve
                 </ListGroup.Item>
             </ListGroup>
         </div>
