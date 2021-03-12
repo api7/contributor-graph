4682a6a4aa26754f59caac0c9202ebe8784d3e91
litesun <7sunmiao@gmail.com>
2021-03-12 15:23:13 +0800

    feat: update css

--- src/App.js
+++ src/App.js
@@ -179,8 +179,7 @@ function App() {
     <>
       <link
         rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
+        href="https://static.apiseven.com/bootstrap.min.css"
         crossorigin="anonymous"
       />
       <div
