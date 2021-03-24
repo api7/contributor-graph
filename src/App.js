03e9a413691d66df77b823d83d026464b9c02840
litesun <7sunmiao@gmail.com>
2021-03-24 12:25:15 +0800

    feat: improve structure

--- src/App.js
+++ src/App.js
@@ -21,66 +21,11 @@ import ShareIcon from "@material-ui/icons/Share";
 import MuiAlert from "@material-ui/lab/Alert";
 
 import Chips from "./components/chip";
+import { getMonths, getParameterByName, isSameDay } from "./utils";
+import { DEFAULT_OPTIONS } from "./constants";
 
-function Alert(props) {
+const Alert = props => {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
-
-const getMonths = (month = 12) => {
-  const d = new Date();
-  const result = [];
-  for (let i = 0; i < month; i++) {
-    d.setMonth(d.getMonth() - 1);
-    const m = d.getMonth() + 1;
-    const month = m < 10 ? `0${m}` : m;
-    result.push(`${d.getFullYear()}-${month}`);
-  }
-  return result.sort();
-};
-
-const getParameterByName = (name, url = window.location.href) => {
-  // eslint-disable-next-line
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
-};
-
-const isSameDay = (d1, d2) => {
-  return (
-    d1.getFullYear() === d2.getFullYear() &&
-    d1.getMonth() === d2.getMonth() &&
-    d1.getDate() === d2.getDate()
-  );
-};
-
-const DEFAULT_OPTIONS = {
-  legend: {
-    top: "5%",
-    data: []
-  },
-  toolbox: {
-    feature: {
-      saveAsImage: {}
-    }
-  },
-  dataset: [],
-  title: {
-    text: "Contributor Over Time"
-  },
-  tooltip: {
-    trigger: "axis"
-  },
-  xAxis: {
-    type: "time",
-    nameLocation: "middle"
-  },
-  yAxis: {
-    name: ""
-  },
-  series: []
 };
 
 const useStyles = makeStyles(theme => ({
@@ -111,7 +56,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-function App() {
+const App = () => {
   const [loading, setLoading] = React.useState(false);
   const [dataSource, setDataSource] = React.useState({});
   const [activeDate, setActiveDate] = React.useState("max");
@@ -129,6 +74,7 @@ function App() {
     setAlertType(type);
     setOpen(true);
   };
+
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
       return;
@@ -465,7 +411,7 @@ function App() {
                       </div>
                       <ReactECharts
                         option={option}
-                        opts={{renderer: 'svg'}}
+                        opts={{ renderer: "svg" }}
                         ref={e => {
                           if (e) {
                             const echartInstance = e.getEchartsInstance();
@@ -487,6 +433,6 @@ function App() {
       </div>
     </>
   );
-}
+};
 
 export default App;
---
+++ src/constants.js
@@ -0,0 +1,26 @@
+export const DEFAULT_OPTIONS = {
+  legend: {
+    top: "5%",
+    data: []
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
+    }
+  },
+  dataset: [],
+  title: {
+    text: "Contributor Over Time"
+  },
+  tooltip: {
+    trigger: "axis"
+  },
+  xAxis: {
+    type: "time",
+    nameLocation: "middle"
+  },
+  yAxis: {
+    name: ""
+  },
+  series: []
+};
