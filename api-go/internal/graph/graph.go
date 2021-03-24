461d232c2bec406e55a41a5b55358186183b57a4
yiyiyimu <wosoyoung@gmail.com>
2021-03-24 21:34:32 +0800

    fix error & temp use sleep instead of waiting render finish
    Signed-off-by: yiyiyimu <wosoyoung@gmail.com>

--- api-go/internal/graph/graph.go
+++ api-go/internal/graph/graph.go
@@ -1,13 +1,26 @@
 package graph
 
 import (
+	"net/url"
+	"strings"
+	"time"
+
 	"github.com/go-rod/rod"
 )
 
 func GetSVG(repo string) string {
-	page := rod.New().MustConnect().MustPage("https://contributor-graph.apiseven.com/?repo=apache/apisix/").MustWindowFullscreen()
+	path := "https://contributor-graph.apiseven.com/?repo=" + repo
+	page := rod.New().MustConnect().MustPage(path).MustWindowFullscreen()
+
+	time.Sleep(5 * time.Second)
 
 	svg := page.MustWaitLoad().MustEval("window.echartInstance.getDataURL()").Str()
 
-	return svg
+	svg, err := url.QueryUnescape(svg)
+	if err != nil {
+		panic(err)
+	}
+
+	strList := strings.Split(svg, "data:image/svg xml;charset=UTF-8,")
+	return strList[1]
 }
