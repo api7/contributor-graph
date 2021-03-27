e3892faa0c95775424965b5be4b07c6858b02370
yiyiyimu <wosoyoung@gmail.com>
2021-03-27 19:09:55 +0800

    feat: add API to return cached repos
    Signed-off-by: yiyiyimu <wosoyoung@gmail.com>

--- api-go/cmd/contributor/main.go
+++ api-go/cmd/contributor/main.go
@@ -16,16 +16,23 @@ import (
 // TODO
 // 怎么存token，留token的API
 
-type returnObj struct {
+type returnConObj struct {
 	Code         int               `json:"code`
 	ErrorMessage string            `json:"message`
 	Contributors []utils.ReturnCon `json:"contributors`
+}
+
+type returnRepoObj struct {
+	Code         int      `json:"code`
+	ErrorMessage string   `json:"message`
+	Repos        []string `json:"repos`
 }
 
 func main() {
 	http.HandleFunc("/contributors", getContributor)
 	http.HandleFunc("/contributors-svg", getContributorSVG)
 	http.HandleFunc("/refreshAll", refreshAll)
+	http.HandleFunc("/repos", getRepos)
 
 	//port := os.Getenv("PORT")
 	port := "8080"
@@ -48,11 +55,11 @@ func getContributor(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		w.WriteHeader(code)
-		json.NewEncoder(w).Encode(returnObj{Code: code, ErrorMessage: err.Error()})
+		json.NewEncoder(w).Encode(returnConObj{Code: code, ErrorMessage: err.Error()})
 		return
 	}
 
-	json.NewEncoder(w).Encode(returnObj{Contributors: conList})
+	json.NewEncoder(w).Encode(returnConObj{Contributors: conList})
 }
 
 func getContributorSVG(w http.ResponseWriter, r *http.Request) {
@@ -68,12 +75,27 @@ func getContributorSVG(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, svg)
 }
 
+func getRepos(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	repos, code, err := gcpdb.GetRepoList()
+
+	if err != nil {
+		w.WriteHeader(code)
+		json.NewEncoder(w).Encode(returnRepoObj{Code: code, ErrorMessage: err.Error()})
+		return
+	}
+
+	json.NewEncoder(w).Encode(returnRepoObj{Repos: repos})
+}
+
 func refreshAll(w http.ResponseWriter, r *http.Request) {
 	_, code, err := gcpdb.UpdateDB("")
 
 	if err != nil {
 		w.WriteHeader(code)
-		json.NewEncoder(w).Encode(returnObj{Code: code, ErrorMessage: err.Error()})
+		json.NewEncoder(w).Encode(returnConObj{Code: code, ErrorMessage: err.Error()})
 		return
 	}
 }
