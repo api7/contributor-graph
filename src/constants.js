7f002b1eeca7bd112249ca1f78a42bda852c39e3
litesun <sunyi@apache.org>
2021-04-04 17:05:07 +0800

    feat: add contributor activity line (#23)

--- src/constants.js
+++ src/constants.js
@@ -24,3 +24,30 @@ export const DEFAULT_OPTIONS = {
   },
   series: []
 };
+
+export const DEFAULT_ACTIVITY_OPTIONS = {
+  legend: {
+    top: "5%",
+    data: []
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
+    }
+  },
+  dataset: [],
+  title: {
+    text: "Contributor Monthly Activity"
+  },
+  tooltip: {
+    trigger: "axis"
+  },
+  xAxis: {
+    type: "time",
+    nameLocation: "middle"
+  },
+  yAxis: {
+    name: ""
+  },
+  series: []
+};
