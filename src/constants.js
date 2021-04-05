ee7435808897dd40866e8f7a2d5def103a021ece
琚致远 <juzhiyuan@apache.org>
2021-04-05 10:03:43 +0800

    Revert "feat: add contributor activity line (#23)" (#24)
    This reverts commit 7f002b1eeca7bd112249ca1f78a42bda852c39e3.

--- src/constants.js
+++ src/constants.js
@@ -24,30 +24,3 @@ export const DEFAULT_OPTIONS = {
   },
   series: []
 };
-
-export const DEFAULT_ACTIVITY_OPTIONS = {
-  legend: {
-    top: "5%",
-    data: []
-  },
-  toolbox: {
-    feature: {
-      saveAsImage: {}
-    }
-  },
-  dataset: [],
-  title: {
-    text: "Contributor Monthly Activity"
-  },
-  tooltip: {
-    trigger: "axis"
-  },
-  xAxis: {
-    type: "time",
-    nameLocation: "middle"
-  },
-  yAxis: {
-    name: ""
-  },
-  series: []
-};
