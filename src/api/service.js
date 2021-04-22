e7f296264713b0acc15d4023cddeb99f9f24c61d
litesun <7sunmiao@gmail.com>
2021-04-22 14:39:08 +0800

    feat: add cache

--- src/api/service.js
+++ src/api/service.js
@@ -1,5 +1,9 @@
+const cacheRepoList = {};
 export const getGithubRepoList = searchText => {
   return new Promise((resolve, reject) => {
+    if (cacheRepoList[searchText]) {
+      resolve(cacheRepoList[searchText]);
+    }
     fetch(`https://api.github.com/search/repositories?q=${searchText}`)
       .then(response => {
         return response.json();
@@ -8,6 +12,9 @@ export const getGithubRepoList = searchText => {
         const filteredData = myJson.items
           .filter(item => item.full_name.startsWith(searchText))
           .map(item => item.full_name);
+        if (!cacheRepoList[searchText]) {
+          cacheRepoList[searchText] = filteredData;
+        }
         resolve(filteredData);
       })
       .catch(e => {
