e3abf278d002d4c57fa3c7f706b2d159c0c0271a
Young <iskyex@outlook.com>
2022-05-12 09:58:59 +0800

    feat: allow download iamges (#187)

--- api-go/cmd/contributor/main.go
+++ api-go/cmd/contributor/main.go
@@ -132,6 +132,7 @@ func getContributorSVG(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Add("content-type", "image/svg+xml;charset=utf-8")
 	w.Header().Add("cache-control", "public, max-age=86400")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	svg = strings.Replace(svg, "%", "%%", -1)
 	fmt.Fprintf(w, svg)
